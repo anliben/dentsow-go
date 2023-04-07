97bf1ffcca0e070fff13af94fe5d719dcf11f001
anliben <anliben.yayoi@gmail.com>
2023-04-07 20:37:33 -0300

    aumentando timeout caixa

--- pkg/utils/controller.go
+++ pkg/utils/controller.go
@@ -1,7 +1,12 @@
 package utils
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/timeout"
 	"gorm.io/gorm"
 )
 
@@ -14,8 +19,30 @@ func RegisterRoutes(app *fiber.App, db *gorm.DB) {
 		Db: db,
 	}
 
+	h := func(c *fiber.Ctx) error {
+		sleepTime, _ := time.ParseDuration(c.Params("sleepTime") + "ms")
+		if err := sleepWithContext(c.UserContext(), sleepTime); err != nil {
+			return fmt.Errorf("%w: execution error", err)
+		}
+		return nil
+	}
+
 	routes := app.Group("/api/v1/utils")
 	routes.Get("/:table", r.GetCountIdTable)
-	routes.Get("/:mes/:ano", r.GetCaixaEnd)
+	routes.Get("/:mes/:ano", r.GetCaixaEnd, timeout.New(h, 10*time.Second))
 	routes.Get("/migrate", r.Migrate)
+}
+
+func sleepWithContext(ctx context.Context, d time.Duration) error {
+	timer := time.NewTimer(d)
+
+	select {
+	case <-ctx.Done():
+		if !timer.Stop() {
+			<-timer.C
+		}
+		return context.DeadlineExceeded
+	case <-timer.C:
+	}
+	return nil
 }
