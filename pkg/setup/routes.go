48f9422e63ffc33cde68396db395406a9cc804da
anliben <anliben.yayoi@gmail.com>
2023-04-14 17:42:36 -0300

    fix(db): concertando database

--- pkg/setup/routes.go
+++ pkg/setup/routes.go
@@ -14,24 +14,12 @@ import (
 
 	//"fiber/pkg/common/models"
 
-	_ "fiber/docs"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
-// @title Fiber Example API
-// @version 1.0
-// @description This is a sample swagger for Fiber
-// @termsOfService http://swagger.io/terms/
-// @contact.name API Support
-// @contact.email fiber@swagger.io
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-// @host localhost:8080
-// @BasePath /
 func Setup() {
 	app := fiber.New(fiber.Config{
 		JSONEncoder: json.Marshal,
