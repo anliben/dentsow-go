4ae36483dc7626da333a39894bba4ba76660bcf4
João Victor <anliben.yayoi@gmail.com>
2023-07-01 00:59:26 -0300

    fix(atendimento): filtros

--- pkg/budget/get_all.go
+++ pkg/budget/get_all.go
@@ -2,6 +2,7 @@ package budget
 
 import (
 	"fiber/pkg/common/models"
+	"fiber/pkg/utils"
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
@@ -9,7 +10,29 @@ import (
 
 func (r handler) GetAll(app *fiber.Ctx) error {
 	var budget []models.Budget
-	err := r.Db.Preload("Cliente").Preload("Cliente.Midia").Preload("Vendedor").Preload("Vendedor.Groups").Preload("Arquivos").Preload("Procedure").Preload("ValorProposta").Find(&budget).Error
+
+	data := app.Query("data")
+	situacao := app.Query("situacao")
+	anotacoes := app.Query("anotacoes")
+	forma_pagamento := app.Query("forma_pagamento")
+	valor_total := app.Query("valor_total")
+
+	db := r.Db.
+		Preload("Cliente").
+		Preload("Cliente.Midia").
+		Preload("Vendedor").
+		Preload("Vendedor.Groups").
+		Preload("Arquivos").
+		Preload("Procedure").
+		Preload("ValorProposta")
+
+	db.Where(utils.Builder("data LIKE ?", "%"+data+"%"))
+	db.Where(utils.Builder("situacao LIKE ?", "%"+situacao+"%"))
+	db.Where(utils.Builder("anotacoes LIKE ?", "%"+anotacoes+"%"))
+	db.Where(utils.Builder("forma_pagamento LIKE ?", "%"+forma_pagamento+"%"))
+	db.Where(utils.Builder("valor_total LIKE ?", "%"+valor_total+"%"))
+
+	err := db.Find(&budget).Error
 
 	if err != nil {
 		app.Status(http.StatusUnprocessableEntity).JSON(&fiber.Map{
